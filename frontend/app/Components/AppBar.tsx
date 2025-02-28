993b9eae2d4bfad25e35a96a4ceaf6e017591ec7
Akash R <84657690+akshlyfy03@users.noreply.github.com>
2025-02-28 18:29:42 +0530

    User modified

--- frontend/app/Components/AppBar.tsx
+++ frontend/app/Components/AppBar.tsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from "react"
 export const AppBar = () => {
     const [isLogin,setIsLogin] = useState(false);
     const router = useRouter();
-    const userId = localStorage.getItem("userId")
+    const userId = localStorage.getItem("userId");
+    
     useEffect(()=>{
         if (userId) {
             setIsLogin(true)
@@ -13,27 +14,23 @@ export const AppBar = () => {
     },[])
     return <div className="flex border-b justify-between p-4">
         <div className="flex flex-col justify-center text-2xl font-extrabold ">
-            <div className="flex gap-40">
-            <div>
+            <div className="flex gap-10">
+            <div className="">
                 <img src="" alt="logo" />
             </div>
-            <div>
+            <div className="">
                 Social Media App
             </div>
             <div>
                 {isLogin && (
-                        <div className="relative inline-block">
-                            <button className="btn btn-solid-primary my-2 peer">profile Pic</button>
-                            <div className="hidden peer-focus:block absolute bg-white shadow-lg rounded-md mt-2 w-48">
-                                <a className="block px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer" href="/user">Profile</a>
-                                <a className="block px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer">Account settings</a>
-                                <a className="block px-4 py-2 text-sm hover:bg-gray-100 cursor-pointer">Subscriptions</a>
-                            </div>
-                        </div>  
-                
+                        <div className="">
+                            <img src="" alt="profile pic" onClick={()=>{
+                                router.push("/user/id?="+userId)
+                            }}/>
+                        </div> 
                 )}
             </div>
-            </div>
+            </div>  
         </div>
     </div>
 }
--- frontend/app/user/[...]/page.tsx
+++ frontend/app/user/[...]/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { useParams, useSearchParams } from "next/navigation";
+
 export default function () {
-    const userId = ""
+    const searchParams = useSearchParams()
+    const userId = searchParams.get("id");
+    console.log(userId);
     const name = localStorage.getItem("name");
     return <div className="flex justify-center items-center h-screen">
         <div className="flex w-fit flex-col border border-slate-200 shadow p-12 rounded-xl">
             <div className="flex justify-between gap-60">
                 <div>
                     {name}
+                    {userId}
                 </div>
                 <div>
                     {name}
