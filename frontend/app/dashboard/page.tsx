d110b10cff36e4888f094919bdd770bf1b982288
Akash R <84657690+akshlyfy03@users.noreply.github.com>
2025-03-04 12:16:21 +0530

    finished implementation of post,feed,like,comment

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/navigation";
 import { AppBar } from "../Components/AppBar";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -19,8 +18,6 @@ export default function(){
     const [comment,setComment] = useState<string>();
     const [storePostId,setStorePostId] = useState<string|null>(null);
     const [showComments,setShowComments] = useState<any[]>([]);
-    const [refresh, setRefresh] = useState(false);
-    const router = useRouter();
     const getAllPost = async () => {
         try {
             if (!token) {
@@ -35,6 +32,7 @@ export default function(){
     
             setFeed(res.data.getAllPost || []);
             setIsLogin(true);
+            setComment("")
         } catch (err) {
             console.error("Error fetching posts:", err);
         }
@@ -136,9 +134,8 @@ export default function(){
                 userId
             })
             if (res.data) {
+                setComment((prev)=>"");
                 console.log("commented");
-                setComment("");
-                setRefresh(prev => !prev); 
                 getAllComments({postId});
                 getAllPost();
             }
@@ -152,7 +149,6 @@ export default function(){
             <div className="flex w-fit flex-col border border-slate-200 shadow p-2 rounded-xl  h-[80vh]">
                 <div className="p-2">
                         <AppBar/>
-                        {}
                     </div>
                     {isLogin && (
                         <div className="max-h-[80vh] overflow-y-auto p-5">
@@ -251,14 +247,14 @@ export default function(){
                                                                                 type="text"
                                                                                 placeholder="Write a caption..."
                                                                                 className="border border-slate-300 rounded-lg p-1 w-full mb-3 "
+                                                                                value={comment}
                                                                                 onChange={(e) => setComment(e.target.value)}
                                                                             />
                                                                             <button
                                                                                 className="pb-2 pl-2"
                                                                                 onClick={()=>{
-                                                                                    if (!storePostId) return
+                                                                                    if (!storePostId) return false;
                                                                                     handleComment({postId:storePostId})
-                                                                                    // setShowCommentModel(false);
                                                                                     }}>
                                                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
                                                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
